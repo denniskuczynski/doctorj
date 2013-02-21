b05bfd4c4a29dafe43cdce864a426ebb80ea00b6
Dennis Kuczynski <dennis.kuczynski@gmail.com>
2013-02-21 16:13:27 -0500

    Initial skeleton of app (Basically just playing with the LibreOffice API examples)

---
+++ README.md
@@ -0,0 +1,28 @@
+# doctorj
+
+Simple server to connect to LibreOffice and convert documents to PDF
+
+## Installation
+
+1. Download and install LibreOffice_4.0.0
+2. Download and install buildr
+3. clone this git repository
+4. In the root directory, execute: buildr run
+5. Use the server API to post files and download PDFs
+
+## Testing
+
+1. In the root directory, execute: buildr test
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Added some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+## Licensing
+
+LibreOffice is released under the LGPL
+http://www.libreoffice.org/download/license/
---
+++ src/main/java/DoctorJ.java
@@ -0,0 +1,105 @@
+import com.sun.star.uno.UnoRuntime;
+import java.io.File;
+
+public class DoctorJ {
+
+    static com.sun.star.frame.XComponentLoader xCompLoader = null;
+
+    static void convertDocumentToPDF(File document, File outputDir) 
+        throws com.sun.star.io.IOException, 
+               com.sun.star.util.CloseVetoException {
+        String inputURL = getFileURL(document);
+        String outputURL = getFileURL(outputDir);
+        com.sun.star.frame.XStorable xStorable = openDocument(inputURL);
+        processDocument(xStorable, inputURL, outputURL);
+        closeDocument(xStorable);
+    }
+
+    static String getFileURL(File file) {
+        return "file:///" + file.getAbsolutePath().replace( '\\', '/' );
+    }
+
+    // Return an object that will offer a simple way to store a document to a URL.    
+    static com.sun.star.frame.XStorable openDocument(String inputURL)
+        throws com.sun.star.io.IOException {
+        com.sun.star.beans.PropertyValue propertyValues[] =
+            new com.sun.star.beans.PropertyValue[1];
+        propertyValues[0] = new com.sun.star.beans.PropertyValue();
+        propertyValues[0].Name = "Hidden";
+        propertyValues[0].Value = new Boolean(true);
+
+        Object oDocToStore =
+            DoctorJ.xCompLoader.loadComponentFromURL(
+                inputURL, "_blank", 0, propertyValues);
+        
+        com.sun.star.frame.XStorable xStorable =
+            (com.sun.star.frame.XStorable)UnoRuntime.queryInterface(
+                com.sun.star.frame.XStorable.class, oDocToStore );
+        System.out.println("Opened document: "+inputURL);
+        return xStorable;
+    }
+
+    static void processDocument(com.sun.star.frame.XStorable xStorable, String inputURL, String outputURL) 
+        throws com.sun.star.io.IOException {
+        com.sun.star.beans.PropertyValue propertyValues[] = 
+            new com.sun.star.beans.PropertyValue[2];
+        propertyValues[0] = new com.sun.star.beans.PropertyValue();
+        propertyValues[0].Name = "Overwrite";
+        propertyValues[0].Value = new Boolean(true);
+        propertyValues[1] = new com.sun.star.beans.PropertyValue();
+        propertyValues[1].Name = "FilterName";
+        propertyValues[1].Value = "writer_pdf_Export";
+
+        // Appending the favoured extension to the origin document name
+        int index1 = inputURL.lastIndexOf('/');
+        int index2 = inputURL.lastIndexOf('.');
+        String storeToURL = outputURL + inputURL.substring(index1, index2 + 1) + "pdf";
+
+        // Storing and converting the document
+        xStorable.storeToURL(storeToURL, propertyValues);
+        System.out.println("Wrote file to: "+outputURL);
+    }
+
+    // Closing the converted document. Use XCloseable.clsoe if the
+    // interface is supported, otherwise use XComponent.dispose    
+    static void closeDocument(com.sun.star.frame.XStorable xStorable)
+       throws com.sun.star.util.CloseVetoException {
+        com.sun.star.util.XCloseable xCloseable =
+            (com.sun.star.util.XCloseable)UnoRuntime.queryInterface(
+                com.sun.star.util.XCloseable.class, xStorable);
+        if ( xCloseable != null ) {
+            xCloseable.close(false);
+        } else {
+            com.sun.star.lang.XComponent xComp =
+                (com.sun.star.lang.XComponent)UnoRuntime.queryInterface(
+                    com.sun.star.lang.XComponent.class, xStorable);
+
+            xComp.dispose();
+        }
+        System.out.println("Closed document");
+    }
+
+    public static void main( String args[] ) {
+        com.sun.star.uno.XComponentContext xContext = null;
+        try {
+            xContext = com.sun.star.comp.helper.Bootstrap.bootstrap();
+            System.out.println("Connected to a running office ...");
+
+            com.sun.star.lang.XMultiComponentFactory xMCF = 
+                xContext.getServiceManager();
+
+            Object oDesktop = xMCF.createInstanceWithContext(
+                "com.sun.star.frame.Desktop", xContext);
+
+            xCompLoader = (com.sun.star.frame.XComponentLoader)
+                UnoRuntime.queryInterface(com.sun.star.frame.XComponentLoader.class,
+                                          oDesktop);
+
+            convertDocumentToPDF(new File("./data/example_docs/test1.odt"), new File("./data/output_docs"));
+            System.exit(0);
+        } catch( Exception e ) {
+            e.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
+}
